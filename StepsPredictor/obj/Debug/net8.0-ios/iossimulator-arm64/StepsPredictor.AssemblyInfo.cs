03093469d976911256534ab29a053f431d32101f
Jiaojiao Song <ssssjulia525@gmail.com>
2024-09-30 15:19:32 +0200

    Revert "data exploration"
    This reverts commit 32dfb734239c84ce3ee0fa6d32eeb9d2ecebaa27.

--- StepsPredictor/obj/Debug/net8.0-ios/iossimulator-arm64/StepsPredictor.AssemblyInfo.cs
+++ StepsPredictor/obj/Debug/net8.0-ios/iossimulator-arm64/StepsPredictor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StepsPredictor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+17e7644c4dea27291773f9b11fe41b315f23a171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c7915dcb10827c63e6d11792b8dfffa21e91a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("StepsPredictor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StepsPredictor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
